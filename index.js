d8ff3b8952cc4cbb01b2870de2829ca803cca089
jjavimu <jmulero@ucm.es>
2022-05-14 12:44:08 +0200

    limit 50mb json

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ require("dotenv").config({ path: "./.env" });
 
 // Settings
 const port = process.env.PORT || 5000;
+app.use(express.json({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Midedlewares
 app.use(cors());
